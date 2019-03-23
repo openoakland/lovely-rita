0d283266d39554a16fa4a200b58c0e7c63bd0bee
Robert Gibboni <galileo@gmail.com>
2019-03-23 19:42:53 +0000

    Refactor config loading to work cross platform

--- lovelyrita/config.py
+++ lovelyrita/config.py
@@ -1,29 +1,21 @@
 import os
-import os.path as op
 import json
+from pathlib import Path
+
+import appdirs
 
 import lovelyrita
 
-DEFAULT_CONFIG_PATH = op.join(op.dirname(lovelyrita.__file__), 'options.json')
+CONFIG_DIRECTORY = Path(appdirs.user_config_dir()) / 'lovelyrita'
+CONFIG_DIRECTORY.mkdir(exist_ok=True)
+CONFIG_PATH = CONFIG_DIRECTORY / 'options.json'
 
-with open(DEFAULT_CONFIG_PATH, 'r') as f:
-    config = json.load(f)
+if CONFIG_PATH.exists():
+    with open(CONFIG_PATH, 'r') as f:
+        config = json.load(f)
 
-CONFIG_DIRECTORY = op.expanduser('~/.config/lovelyrita')
-
-if not op.exists(CONFIG_DIRECTORY):
-    os.makedirs(CONFIG_DIRECTORY)
-
-CONFIG_PATH = op.join(CONFIG_DIRECTORY, 'options.json')
-
-if not op.exists(CONFIG_PATH):
-    with open(CONFIG_PATH, 'w') as f:
-        json.dump({}, f)
-
-with open(CONFIG_PATH, 'r') as f:
-    user_config = json.load(f)
-
-config.update(user_config)
+else:
+    config = {}
 
 GOOGLE_API_KEY = config.get('GOOGLE_API_KEY', None)
 GOOGLE_API_URL = config.get('GOOGLE_API_URL', None)
